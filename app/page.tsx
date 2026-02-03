cef80123f6c9361ac1188cd6196aaba64bf379f3
sasha <sayces@mail.ru>
2026-02-03 21:32:56 +0300

    s

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,9 @@ export default function Home() {
     <div className={`app bg-gray-29`}>
       <Header />
       <main>
-        <h1>Выбери подходящий для себя <span className="text-golden">тариф</span></h1>
+        <h1>
+          Выбери подходящий для себя <span className="text-golden">тариф</span>
+        </h1>
       </main>
     </div>
   );
